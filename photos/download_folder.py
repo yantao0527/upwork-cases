921b4624e660d1328b3f55c434ee424c153be01f
frank <yantao0527@gmail.com>
2023-04-26 14:36:57 +0000

    fix and improve

--- photos/download_folder.py
+++ photos/download_folder.py
@@ -16,30 +16,42 @@ expiry_hours = 1  # Set the expiry time in hours
 # Create the BlobServiceClient object
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
+def zip_exists(folder_name):
+    try:
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=f'{folder_name}.zip')
+        blob_client.get_blob_properties()
+        return True
+    except Exception as e:
+        return False
+
 @app.route('/download-folder')
 def download_folder():
     # Get the folder name from the request arguments
     folder_name = request.args.get('folder_name')
-    
-    # Create a temporary file to store the zip file
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    
-    # Create the zip file and add all files in the folder to it
-    with zipfile.ZipFile(temp_file, 'w', zipfile.ZIP_DEFLATED) as zip:
-        for blob in blob_service_client.get_container_client(container_name).list_blobs(name_starts_with=folder_name):
-            blob_client = blob_service_client.get_blob_client(container_name, blob.name)
-            blob_data = blob_client.download_blob().readall()
-            file_name = os.path.basename(blob.name)
-            zip.writestr(file_name, blob_data)
-    
-    # Close the temporary file
-    temp_file.close()
 
-    # store zip file to ABS
-    with open(temp_file, "rb") as data:
-        blob_client = blob_service_client.get_blob_client(container=container_name, blob=f'{folder_name}.zip')
-        blob_client.upload_blob(data)
-    
+    if not zip_exists(folder_name):
+        # Create a temporary file to store the zip file
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+
+        # Create the zip file and add all files in the folder to it
+        with zipfile.ZipFile(temp_file, 'w', zipfile.ZIP_DEFLATED) as zip:
+            for blob in blob_service_client.get_container_client(container_name).list_blobs(name_starts_with=folder_name):
+                blob_client = blob_service_client.get_blob_client(container_name, blob.name)
+                blob_data = blob_client.download_blob().readall()
+                file_name = os.path.basename(blob.name)
+                zip.writestr(file_name, blob_data)
+
+        # Close the temporary file
+        temp_file.close()
+
+        # Store zip file to ABS
+        with open(temp_file.name, "rb") as data:
+            blob_client = blob_service_client.get_blob_client(container=container_name, blob=f'{folder_name}.zip')
+            blob_client.upload_blob(data)
+
+        # Remove the temporary file
+        os.remove(temp_file.name)
+
     # Get the account name and key from the connection string
     account_name = blob_service_client.account_name
     account_key = connection_string.split("AccountKey=")[1].split(";")[0]
