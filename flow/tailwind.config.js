dc765e2d60712444a6b8b6b027345893b91a109b
frank <yantao0527@gmail.com>
2023-06-09 16:32:02 +0000

    dark mode

--- flow/tailwind.config.js
+++ flow/tailwind.config.js
@@ -8,11 +8,13 @@ module.exports = {
     "./nuxt.config.{js,ts}",
     "./node_modules/flowbite/**/*.js",
   ],
-  darkMode: 'class',
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"},
+        themeBackground: 'var(--background)',
+        themeText: 'var(--text)',
       }
     },
     fontFamily: {
