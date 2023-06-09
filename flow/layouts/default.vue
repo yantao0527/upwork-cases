dda342b75d63501b73b68bc99fdbbd199e5ca5f9
frank <yantao0527@gmail.com>
2023-06-09 16:31:22 +0000

    fix default layout

--- flow/layouts/default.vue
+++ flow/layouts/default.vue
@@ -2,6 +2,8 @@
     <div class="antialiased bg-gray-50 dark:bg-gray-900">
       <Header />
       <Sidebar />
-      <slot />
+      <main class="p-4 md:ml-64 h-auto pt-20">
+        <slot />
+      </main>
     </div>
 </template>
